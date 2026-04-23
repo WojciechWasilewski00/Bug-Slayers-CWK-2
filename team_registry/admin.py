a1a6360db9220d6e7b6d7d71ef70fabe36dbd9b2
Felipe Salkeld Marrou <w2064636@westminster.ac.uk>
2026-04-23 12:36:17 +0100

    Add team_registry models, views, urls and templates

--- team_registry/admin.py
+++ team_registry/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Team
+from .models import TeamType, Department, Team, Dependency
+
+@admin.register(TeamType)
+class TeamTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'specialisation', 'leader']
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at') #example code so that jayne can run scheduling page
+    list_display = ['name', 'department', 'team_type', 'created_at']
+
+@admin.register(Dependency)
+class DependencyAdmin(admin.ModelAdmin):
+    list_display = ['from_team', 'to_team', 'dependency_type']
--- team_registry/views.py
+++ team_registry/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Department, Team, TeamType, Dependency
 
-# Create your views here.
+def organisation_overview(request):
+    departments = Department.objects.all()
+    teams = Team.objects.all()
+    return render(request, 'team_registry/organisation_overview.html', {
+        'departments': departments,
+        'teams': teams
+    })
+
+def department_detail(request, pk):
+    department = get_object_or_404(Department, pk=pk)
+    teams = Team.objects.filter(department=department)
+    return render(request, 'team_registry/department_detail.html', {
+        'department': department,
+        'teams': teams
+    })
+
+def team_detail(request, pk):
+    team = get_object_or_404(Team, pk=pk)
+    dependencies_from = Dependency.objects.filter(from_team=team)
+    dependencies_to = Dependency.objects.filter(to_team=team)
+    return render(request, 'team_registry/team_detail.html', {
+        'team': team,
+        'dependencies_from': dependencies_from,
+        'dependencies_to': dependencies_to
+    })
